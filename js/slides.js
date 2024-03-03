6c4eab0f4f66117382521ca3c5e419dbdb09fbdb
gabrieldario66@gmail.com <gabrieldario66@gmail.com>
2024-03-03 19:16:55 -0300

    Feito site inteiro,falta ajustar ,alinhar, botar dados corretos e ajustar para celular

--- js/slides.js
+++ js/slides.js
@@ -1,13 +1,14 @@
 
 let index = 0;
 displayImages();
+crateCanvas();
 function displayImages() {
     let i;
     const images = document.getElementsByName("image");
 
     for (i = 0; i < images.length; i++) {
         images[i].style.display = "none";
-   
+
     }
     index++;
 
@@ -17,4 +18,13 @@ function displayImages() {
 
     images[index - 1].style.display = "block";
     setTimeout(displayImages, 4000);
+
+}
+function crateCanvas() {
+    var largura = window.innerWidth
+
+    var altura = window.innerHeight
+    console.log(largura);
+    console.log(altura);
+
 }
