0ea886927333ab26867d819d04e560ff07c41ecb
gabrieldario66@gmail.com <gabrieldario66@gmail.com>
2024-03-04 18:17:59 -0300

    Tentando por popUp

---
+++ js/popUps.js
@@ -0,0 +1,18 @@
+let btnAbrir = document.getElementsByClassName('button-43');
+let btnFechar = document.getElementById('fechar-popup-btn');
+let conteudo = document.getElementById('conteudo');
+console.log(btnAbrir)
+
+btnAbrir.addEventListener('click', (e) =>{
+  console.log("Abriu");
+//   btnAbrir.style.display = 'none';
+//   btnFechar.style.display = 'block';
+
+//   conteudo.style.display = 'block';
+})
+// btnFechar.addEventListener('click', (e) => {
+//   console.log("Fechou");
+//   btnAbrir.style.display = 'block';
+//   btnFechar.style.display = 'none';
+//   conteudo.style.display = 'none';
+// })
