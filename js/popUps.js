e7bb9b10626323d7a8a5ae1e8253b68a08549e92
gabrieldario66@gmail.com <gabrieldario66@gmail.com>
2024-03-06 18:24:39 -0300

    ppUP

--- js/popUps.js
+++ js/popUps.js
@@ -1,18 +1,36 @@
 let btnAbrir = document.getElementById('button-43');
 let btnFechar = document.getElementById('fechar-popup-btn');
-let conteudo =  document.getElementById('popUpManuntencao');
+let conteudo = document.getElementById('popUpManuntencao');
+let body = document.body;
+
+let condicao = 0;
 
 
-btnAbrir.addEventListener('click', (e) =>{
-  console.log("Abriu");
-//   btnAbrir.style.display = 'none';
-//   btnFechar.style.display = 'block';
-
-  conteudo.style.display = 'block';
+body.addEventListener('click', (e) => {
+  condicao++;
+  if (condicao == 1) {
+    console.log('Está abrindo');
+  } else if(condicao == 0){
+    console.log('NADA');
+  }else if(condicao >= 2){
+    conteudo.style.display = 'none';
+    condicao = 0;
+  }
 })
-// btnFechar.addEventListener('click', (e) => {
-//   console.log("Fechou");
-//   btnAbrir.style.display = 'block';
-//   btnFechar.style.display = 'none';
-//   conteudo.style.display = 'none';
-// })
+
+
+btnAbrir.addEventListener('click', (e) => {
+  console.log("Abriu");
+  conteudo.style.display = 'block';
+  condicao++;
+
+})
+
+btnFechar.addEventListener('click', (e) => {
+  console.log("Fechou");
+  conteudo.style.display = 'none';
+
+  condicao--;
+})
+
+
