4a461f68d181855f0c3637c048dfc06f6a9df610
gabrieldario66@gmail.com <gabrieldario66@gmail.com>
2024-03-13 18:26:35 -0300

    Ajeitando #conteudo ir sem o fixo

--- js/popUps.js
+++ js/popUps.js
@@ -5,11 +5,9 @@ let conteudo = document.getElementById('popUpManuntencao');
 
 btnAbrir.addEventListener('click', (e) => {
   conteudo.style.display = 'block';
-  condicao++;
 })
 btnFechar.addEventListener('click', (e) => {
   conteudo.style.display = 'none';
-  condicao--;
 })
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
@@ -19,5 +17,42 @@ function myFunction() {
   } else {
     x.style.display = "block";
   }
+}
+
+function myFunctionMove(y) {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+
+  let body = document.body;
+  let contentMenu = document.querySelector('.content');
+
+  let content1 = document.getElementById('content1');
+  let content2 = document.getElementById('content2');
+  let content3 = document.getElementById('content3');
+  let content4 = document.getElementById('content4');
+  let content5 = document.getElementById('content5');
+
+
+  let larguraBody = body.clientWidth;
+  let listaAlturas = [];
+
+  listaAlturas.push(contentMenu.clientHeight);
+  listaAlturas.push(content1.clientHeight);
+  listaAlturas.push(content2.clientHeight);
+  listaAlturas.push(content3.clientHeight);
+  listaAlturas.push(content4.clientHeight);
+  listaAlturas.push(content5.clientHeight);
+
+  let somaAltura = 0;
+  for(i = 0; i <= y; i++) {
+    somaAltura += listaAlturas[i];
+  }
+  window.scrollTo(0, 150);
+
+
 }
 
